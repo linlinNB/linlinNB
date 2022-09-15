64c42bc02536fb06091cd962d574528cf6f5f384
maozhengwei <zhengwei.mao@watrix.ai>
2022-09-15 14:23:18 +0800

    调整依赖

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -99,5 +99,13 @@ module.exports = {
         },
       }
     ]
+  ],
+  markdown: {
+    // lineNumbers: true,
+    extractHeaders: ['h2', 'h3', 'h4', 'h5', 'h6'], // 提取标题到侧边栏的级别，默认['h2', 'h3']
+  },
+  // 监听文件变化并重新构建
+  extraWatchFiles: [
+    '.vuepress/config.js',
   ]
 }
