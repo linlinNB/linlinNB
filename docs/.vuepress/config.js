3c5829c3f358ef06a7762f3e119f58aa11d8d131
maozhengwei <zhengwei.mao@watrix.ai>
2022-09-15 14:21:12 +0800

    添加依赖

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -23,7 +23,14 @@ module.exports = {
       showToArticle: false,
       moreArticle: ''
     },
+    footer: { // 页脚信息
+      createYear: 2022, // 博客创建年份
+      copyrightInfo: 'watrix.ai | MIT License', // 博客版权信息，支持a标签
+    },
     logo: '/images/watrix.png',
+    pageStyle: 'line',
+    sidebarDepth: 2, // 侧边栏显示深度，默认1，最大2（显示到h3标题）
+    searchMaxSuggestions: 10, // 搜索结果显示最大数
     locales: {
       '/': {
         home: '/',
@@ -36,7 +43,7 @@ module.exports = {
         warning: '注意',
         danger: '警告',
         // page meta
-        lastUpdatedText: '上次更新',
+        lastUpdated: '上次更新',
         contributors: false,
         // 404 page
         notFound: [
@@ -56,4 +63,41 @@ module.exports = {
   chainWebpack (config) {
     config.resolve.alias.set('core-js/library/fn', 'core-js/features');
   },
+  plugins: [
+    ['fulltext-search'],
+    ['one-click-copy', { // 代码块复制按钮
+      copySelector: ['div[class*="language-"] pre', 'div[class*="aside-code"] aside'], // String or Array
+      copyMessage: '复制成功', // default is 'Copy successfully and then paste it for use.'
+      duration: 1000, // prompt message display time.
+      showInMobile: false // whether to display on the mobile side, default: false.
+    }],
+    ['demo-block', { // demo演示模块 https://github.com/xiguaxigua/vuepress-plugin-demo-block
+      settings: {
+        // jsLib: ['http://xxx'], // 在线示例(jsfiddle, codepen)中的js依赖
+        // cssLib: ['http://xxx'], // 在线示例中的css依赖
+        // vue: 'https://fastly.jsdelivr.net/npm/vue/dist/vue.min.js', // 在线示例中的vue依赖
+        jsfiddle: false, // 是否显示 jsfiddle 链接
+        codepen: true, // 是否显示 codepen 链接
+        horizontal: false // 是否展示为横向样式
+      }
+    }],
+    [
+      'vuepress-plugin-zooming', // 放大图片
+      {
+        selector: '.theme-vdoing-content img:not(.no-zoom)',
+        options: {
+          bgColor: 'rgba(0,0,0,0.6)'
+        },
+      },
+    ],
+    [
+      '@vuepress/last-updated', // "上次更新"时间格式
+      {
+        transformer: (timestamp, lang) => {
+          const dayjs = require('dayjs') // https://day.js.org/
+          return dayjs(timestamp).format('YYYY/MM/DD, HH:mm:ss')
+        },
+      }
+    ]
+  ]
 }
