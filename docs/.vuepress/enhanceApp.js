07a4f4c9eb15c7897f108ef34e611b92b98a79d5
maozhengwei <zhengwei.mao@watrix.ai>
2022-09-21 15:42:52 +0800

    调整目录结构

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -1,6 +1,6 @@
 import Antd from 'ant-design-vue'
 import 'ant-design-vue/dist/antd.css'
-import WatrixComponent from '../../components/global.js'
+import WatrixComponent from '../../packages/components/global.js'
 import InnerComponent from './components/register.js'
 import './styles/themes/index.less'
 
